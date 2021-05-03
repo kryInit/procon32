7f16b1b20ce76338971fdf7d5bd944f408cb6af7
kryInit <kryinit@gmail.com>
2021-05-03 14:31:23 +0900

    実装難しすぎ, メモ代わり

--- algo/src/image_restorer/image_restorer.cpp
+++ algo/src/image_restorer/image_restorer.cpp
@@ -5,12 +5,25 @@
 using namespace std;
 
 #define rep(i,n) for(int i=0; i < static_cast<int>(n); ++i)
+#define repo(i,o,n) for(int i=static_cast<int>(o); i < static_cast<int>(n); ++i)
+
+template<class T>
+using rev_priority_queue = priority_queue<T, vector<T>, greater<T>>;
+
+enum class Direction { U, R, D, L };
+std::ostream& operator << (std::ostream& os, const Direction& dir) {
+    if (dir == Direction::U) os << "U";
+    else if (dir == Direction::R) os << "R";
+    else if (dir == Direction::D) os << "D";
+    else if (dir == Direction::L) os << "L";
+    return os;
+}
 
 class Settings {
-    unsigned int frag_size;
+    unsigned int frag_size{};
     Vec2<unsigned int> div_num;
-    unsigned int selectable_times;
-    unsigned int choice_cost, repl_cost;
+    unsigned int selectable_times{};
+    unsigned int choice_cost{}, repl_cost{};
 
 public:
     void load(const string& path) {
@@ -33,11 +46,11 @@ public:
         cout << left << setw(w) << "choice cost"  << ": " << choice_cost << endl;
         cout << left << setw(w) << "replacement cost"  << ": " << repl_cost << endl;
     }
-    const unsigned int& FRAG_SIZE() const { return frag_size; }
-    const Vec2<unsigned int>& DIV_NUM() const { return div_num; }
-    const unsigned int& SELECTABLE_TIMES() const { return selectable_times; }
-    const unsigned int& CHOICE_COST() const { return choice_cost; }
-    const unsigned int& REPL_COST() const { return repl_cost; }
+    [[nodiscard]] const unsigned int& FRAG_SIZE() const { return frag_size; }
+    [[nodiscard]] const Vec2<unsigned int>& DIV_NUM() const { return div_num; }
+    [[nodiscard]] const unsigned int& SELECTABLE_TIMES() const { return selectable_times; }
+    [[nodiscard]] const unsigned int& CHOICE_COST() const { return choice_cost; }
+    [[nodiscard]] const unsigned int& REPL_COST() const { return repl_cost; }
 
 };
 
@@ -51,13 +64,15 @@ public:
         if (img_data != nullptr) {
             delete[] *img_data;
             delete[] img_data;
+            img_data = nullptr;
         }
     }
-    const unsigned int& FRAGMENT_SIZE() const { return FRAG_SIZE; }
+    [[nodiscard]] const unsigned int& FRAGMENT_SIZE() const { return FRAG_SIZE; }
     void load(const string& frag_path, const Settings& settings) {
         if (img_data != nullptr) {
             delete[] *img_data;
             delete[] img_data;
+            img_data = nullptr;
         }
 
         // allocate memory
@@ -123,6 +138,7 @@ public:
         if (img_frags != nullptr) {
             delete[] *img_frags;
             delete[] img_frags;
+            img_frags = nullptr;
         }
     }
 
@@ -130,6 +146,7 @@ public:
         if (img_frags != nullptr) {
             delete[] *img_frags;
             delete[] img_frags;
+            img_frags = nullptr;
         }
 
         // allocate memory
@@ -140,7 +157,7 @@ public:
 
         // load data
         rep(y, DIV_NUM.y) rep(x, DIV_NUM.x) {
-            cout << y << " " << x << endl;
+//            cout << y << " " << x << endl;
             stringstream ss;
             ss << frags_dir_path << hex << uppercase << x << y << ".ppm.bin";
             img_frags[y][x].load(ss.str(), settings);
@@ -179,25 +196,45 @@ public:
     }
 };
 
+constexpr unsigned int MAX_DIV_NUM = 16;
 
 struct ImageFragmentState {
-    unsigned int rotation_times;
-    Vec2<unsigned int> orig_coord;
+    unsigned char orig_idx, rotation_times;
 
-    ImageFragmentState() : rotation_times(0), orig_coord() {}
-    ImageFragmentState(unsigned int _rotation_times, Vec2<unsigned int> _orig_coord) : rotation_times(_rotation_times), orig_coord(_orig_coord) {}
+    unsigned int get_side_idx(Direction dir) const {
+        unsigned int result = static_cast<unsigned char>(orig_idx)*4;
+//        result += (4+static_cast<unsigned int>(dir) - rotation_times)%4;
+        if (dir == Direction::U) result += (4-rotation_times)%4;
+        else if (dir == Direction::R) result += (5-rotation_times)%4;
+        else if (dir == Direction::D) result += (6-rotation_times)%4;
+        else /* if (dir == Direction::L) */ result += (3-rotation_times);
+        return result;
+    }
+    ImageFragmentState() : orig_idx(), rotation_times() {}
+    ImageFragmentState(unsigned char _orig_idx, unsigned char _rotation_times) : orig_idx(_orig_idx), rotation_times(_rotation_times) {}
 };
 
 struct ImageState {
-    vector<ImageFragmentState> repl_order;
-    vector<bool> placed;
+    Vec2<unsigned int> now_size;
+    array<array<ImageFragmentState, MAX_DIV_NUM>, MAX_DIV_NUM> states;
+    bitset<MAX_DIV_NUM*MAX_DIV_NUM> placed;
 
-    explicit ImageState() : repl_order(), placed() {}
-    explicit ImageState(unsigned int n) : repl_order(), placed(vector<bool>(n, false)) {}
-    void set_image(unsigned int idx, ImageFragmentState frag_state) {
-        placed[idx] = true;
-        repl_order.push_back(frag_state);
+    void rotate_180deg() {
+
     }
+    void place(Vec2<unsigned int> pos, unsigned char orig_idx, unsigned char rotation_times) {
+        states[pos.y][pos.x].orig_idx = orig_idx;
+        states[pos.y][pos.x].rotation_times = rotation_times;
+        placed[orig_idx] = true;
+        now_size.x = max(now_size.x, pos.x+1);
+        now_size.y = max(now_size.y, pos.y+1);
+    }
+
+    vector<ImageFragmentState> get_side(Direction dir) const {
+
+    }
+
+    ImageState() : now_size{}, orig_idx{}, placed{} {}
 };
 
 class IAdjacencyEvaluator {
@@ -267,7 +304,7 @@ public:
                 rep(k,4) {
                     const unsigned int offset = (i*div_num.x*4+j*4+k)*frag_num*4;
                     double best_adjacency = 1e10;
-                    int best_i, best_j, best_k;
+                    int best_i=-1, best_j=-1, best_k=-1;
                     rep(I,div_num.y) {
                         rep(J,div_num.x) {
                             if (I == i && J == j) continue;
@@ -300,136 +337,128 @@ public:
     }
 
     ImageState solve(double *adjacency, const Settings& settings) override {
-        dump_adjacency_info(adjacency, settings);
+//        dump_adjacency_info(adjacency, settings);
+        cout << "size: " << sizeof(ImageState) << endl;
+        cout << sizeof(unsigned char) << endl;
+        const Vec2<unsigned int> DIV_NUM = settings.DIV_NUM();
+        const unsigned int N = DIV_NUM.y*DIV_NUM.x*4;
 
-        const auto div_num = settings.DIV_NUM();
-        const auto frag_num = div_num.y*div_num.x;
+        struct adj_idx {
+            float adjacency;
+            array<unsigned short, 2> idx;
 
-        timeManager tm(2000);
-        constexpr unsigned int MAX_DIV_NUM = 16;
-        constexpr unsigned int MAX_FRAG_NUM = MAX_DIV_NUM*MAX_DIV_NUM;
-        constexpr unsigned int MAX_STORAGE_SIZE = 1000;
+            adj_idx() : adjacency(), idx() {}
+            adj_idx(float _adjacency, unsigned short idx0, unsigned short idx1) : adjacency(_adjacency), idx{idx0, idx1} {}
+            bool operator<(const adj_idx& another) const {
+                return adjacency < another.adjacency;
+            }
+            bool operator>(const adj_idx& another) const {
+                return another.adjacency < adjacency;
+            }
+        };
 
-        int order_table[MAX_DIV_NUM][MAX_DIV_NUM] = {};
-        Vec2<unsigned int> rev_order_table[MAX_FRAG_NUM] = {};
 
+        // make ordered adjacency
+        vector<vector<adj_idx>> ordered_adjacency(N, vector<adj_idx>(N));
+        rep(idx, N) {
+            repo(IDX, idx, N) {
+                const auto adj = static_cast<float>(*(adjacency + idx*N + IDX));
+                ordered_adjacency[idx][IDX] = adj_idx(adj, idx, IDX);
+                ordered_adjacency[IDX][idx] = adj_idx(adj, IDX, idx);
+            }
+            sort(ordered_adjacency[idx].begin(), ordered_adjacency[idx].end());
+        }
+/*
+        rep(i,DIV_NUM.y) rep(j,DIV_NUM.x) rep(k,4) {
+            adj_idx tmp = ordered_adjacency[i*DIV_NUM.x*4 + j*4 + k].front();
+            cout << '(' << j << ", " << i << ", " << k << ") <=> ";
+            cout << '(' << tmp.idx[1]/4 % DIV_NUM.x << ", " << tmp.idx[1]/4 / DIV_NUM.y << ", " << (int)tmp.idx[1]%4 << ") : " << tmp.adjacency << endl;
+        }
+*/
+
+        constexpr unsigned int MAX_STATE_NUM = 8192;
+        vector<ImageState> now_states;
+//        now_states.reserve(MAX_STATE_NUM);
+        // make initial state (two image fragment)
         {
-            int now = 0;
-            int max_div = max(div_num.x, div_num.y);
-            rep(i, max_div) {
-                if (i < div_num.x) rep(j, min(i, (int)div_num.y)) {
-                    order_table[j][i] = now;
-                    rev_order_table[now] = Vec2<unsigned int>(i, j);
-                    now++;
+            rev_priority_queue<adj_idx> pq;
+            rep(i,N) repo(j,i+1,N) if (ordered_adjacency[i][j].adjacency != INFINITY) pq.push(ordered_adjacency[i][j]);
+            rep(i, MAX_STATE_NUM) {
+                adj_idx tmp = pq.top(); pq.pop();
+                ImageState state;
+                {
+                    const unsigned int idx = tmp.idx[0] / 4, rotation_times = (5-tmp.idx[0]%4)%4;
+                    state.place(Vec2<unsigned int>(0,0), idx, rotation_times);
                 }
-                if (i < div_num.y) rep(j,min(i+1, (int)div_num.x)) {
-                    order_table[i][j] = now;
-                    rev_order_table[now] = Vec2<unsigned int>(j, i);
-                    now++;
+                {
+                    const unsigned int idx = tmp.idx[1] / 4, rotation_times = (3-tmp.idx[1]%4);
+                    state.place(Vec2<unsigned int>(0, 1), idx, rotation_times);
                 }
-            }
-            rep(i,div_num.y) {
-                rep(j,div_num.x) cout << order_table[i][j] << " ";
-                cout << endl;
+                now_states.push_back(state);
+                if (pq.empty()) break;
             }
         }
 
-        vector<ImageState> storage[MAX_FRAG_NUM];
-        priority_queue<pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>>> pq[MAX_FRAG_NUM];
-        rep(i, div_num.y) rep(j,div_num.x) rep(k,4) {
-            ImageState state(frag_num);
-            state.set_image(i*div_num.x+j, ImageFragmentState(k, Vec2<unsigned int>(j, i)));
-            pq[0].push(make_pair(0, storage[0].size()));
-            storage[0].push_back(state);
-        }
+        // beam search
+        const unsigned int STEP_NUM = DIV_NUM.y + DIV_NUM.x - 3;
+        rep(_, STEP_NUM) {
+            vector<ImageState> next_states;
+            rev_priority_queue<adj_idx> pq;
+            rep(i, min(MAX_STATE_NUM, (unsigned int)now_states.size())) {
+                const ImageState& tmp_state = now_states[i];
+                const Vec2<unsigned int>& now_size = tmp_state.now_size;
+                rep(j,4) {
+                    vector<ImageFragmentState> side = tmp_state.get_side(static_cast<Direction>(j));
 
-        int now_step = 0;
-//        while(tm.is_within_time_limit()) {
-        int loop_count = 0;
-        while(true) {
-            loop_count++;
-            if (pq[now_step].empty()) {
-                if (now_step >= 1) {
-                    vector<ImageState>().swap(storage[now_step-1]);
-                }
-                now_step++;
-                loop_count = 0;
-                cout << now_step << endl;
-                continue;
-            }
-            double now_cost = pq[now_step].top().first;
-            int storage_idx = pq[now_step].top().second;
-            pq[now_step].pop();
+                    rep(k, side.size()) {
+                        // calc diff adjacency
+                        float diff_adjacency = 0;
+                        {
+                            auto tmp_placed = tmp_state.placed;
+//                            unsigned int first_adjacent_idx = side[k].orig_idx*4 + (6-side[k].rotation_times)%4;
+                            unsigned int first_adjacent_idx = side[k].get_side_idx(Direction::D);
+                            unsigned int best_first_idx = 0;
+                            rep(l, N) {
+                                if (tmp_placed[ordered_adjacency[first_adjacent_idx][l].idx[0]]/4) {
+                                    best_first_idx = ordered_adjacency[first_adjacent_idx][l].idx[0];
+                                    break;
+                                }
+                            }
+                            tmp_placed[best_first_idx/4] = true;
+                            unsigned int tmp_idx = best_first_idx;
+                            for(int l=k-1; l>=0; --l) {
+                                float min_adjacency = 1e20;
+                                unsigned int best_idx = -1;
 
-            ImageState& state = storage[now_step][storage_idx];
+                                const unsigned int u_idx = side[l].get_side_idx(Direction::D);
+                                const unsigned int r_idx = tmp_idx;
+                                rep(I,N) {
+                                    if (tmp_placed[I/4]) {
+                                        I += 3;
+                                        break;
+                                    }
 
-            int next_set_idx = state.repl_order.size();
-            Vec2<unsigned int> next_set_coord = rev_order_table[next_set_idx];
+                                }
+                                tmp_idx = best_idx;
+                            }
+                            for (int l=k+1; l<side.size(); ++l) {
 
-            rep(i,div_num.y) rep(j,div_num.x) {
-                unsigned int my_idx = i*div_num.x+j;
-                if (state.placed[my_idx]) continue;
-                int best_rotation_times = -1;
-                double best_diff_cost = 1e20;
-                rep(k,4) {
-                    double tmp_diff_cost = 0;
-                    if (next_set_coord.x != 0) {
-                        unsigned int opp_set_order = order_table[next_set_coord.y][next_set_coord.x-1];
-                        auto& opp_coord = state.repl_order[opp_set_order].orig_coord;
-                        unsigned int opp_idx = opp_coord.y*div_num.x + opp_coord.x;
-                        unsigned int opp_adjacent_side = (5-state.repl_order[opp_set_order].rotation_times)%4;
-                        unsigned int opp_offset = (opp_idx*4 + opp_adjacent_side) * frag_num * 4;
-                        tmp_diff_cost += *(adjacency + opp_offset + my_idx*4 + 3-k);
-                    }
-                    if (next_set_coord.y != 0) {
-                        unsigned int opp_set_order = order_table[next_set_coord.y-1][next_set_coord.x];
-                        auto& opp_coord = state.repl_order[opp_set_order].orig_coord;
-                        unsigned int opp_idx = opp_coord.y*div_num.x + opp_coord.x;
-                        unsigned int opp_adjacent_side = (6-state.repl_order[opp_set_order].rotation_times)%4;
-                        unsigned int opp_offset = (opp_idx*4 + opp_adjacent_side) * frag_num * 4;
-                        tmp_diff_cost += *(adjacency + opp_offset + my_idx*4 + (4-k)%4);
-                    }
-                    if (best_diff_cost > tmp_diff_cost) {
-                        best_diff_cost = tmp_diff_cost;
-                        best_rotation_times = k;
+                            }
+                        }
+
+                        pq.emplace(diff_adjacency, i, j*MAX_DIV_NUM+k);
                     }
                 }
-
-                ImageState tmp = state;
-                tmp.set_image(my_idx, ImageFragmentState(best_rotation_times, Vec2<unsigned int>(j, i)));
-                pq[now_step+1].push(make_pair(now_cost+best_diff_cost, storage[now_step+1].size()));
-                storage[now_step+1].push_back(tmp);
             }
-
-//            now_step = (now_step+1)%(frag_num-1);
-            if (now_step && loop_count%200 == 0) {
-                if (now_step >= 1) {
-                    vector<ImageState>().swap(storage[now_step-1]);
-                }
-                if (now_step == frag_num-2) break;
-                else {
-                    now_step++;
-                    loop_count = 0;
-                    cout << now_step << endl;
-                }
-            }
-        }
-
-        rep(i,frag_num) cout << i << ": " << pq[i].size() << endl;
-        cout << pq[frag_num-1].top().first << endl;
-        {
-            int idx = pq[frag_num-1].top().second;
-            ImageState state = storage[frag_num-1][idx];
-            rep(i, div_num.y) {
-                rep(j, div_num.x) {
-                    cout << state.repl_order[order_table[i][j]].orig_coord << " " << state.repl_order[order_table[i][j]].rotation_times << ", ";
-                }
-                cout << endl;
-            }
+            next_states.swap(now_states);
         }
 
 
-        cout << "simple solver" << endl;
+        cout << "hi" << endl;
+        cout << sizeof(unsigned char) << endl;
+        cout << sizeof(float) << endl;
+        cout << sizeof(adj_idx) << endl;
+        cout << sizeof(pair<unsigned char, float>) << endl;
         return ImageState();
     }
 };
@@ -440,20 +469,31 @@ ImageState solver(const Image& img, const Settings& settings) {
     // adjacency : double[DIV_NUM.y][DIV_NUM.x][4][DIV_NUM.y][DIV_NUM.x][4]
     // ans : ImageFragmentState[DIV_NUM.y][DIV_NUM.x]
 
+    stopwatch sw;
+
     ADJACENCY_EVALUATOR adjacency_evaluator;
     SOLVER solver;
 
     const unsigned int N = settings.DIV_NUM().x*settings.DIV_NUM().y*4;
+
+    sw.start();
     auto *adjacency = new double[N*N];
     adjacency_evaluator.get_adjacency(img, settings, adjacency);
+    sw.print_ms();
+    sw.print_sec();
 
+    sw.start();
     ImageState ans = solver.solve(adjacency, settings);
+    cout << "end solver" << endl;
+    sw.print_ms();
+    sw.print_sec();
 
     delete[] adjacency;
     return ans;
 }
 
 int main(int argc, char *argv[]) {
+
 #pragma omp parallel
     {
         std::cout << "Hello World!\n";
@@ -471,6 +511,6 @@ int main(int argc, char *argv[]) {
     img.load(frag_dir_path, settings);
     cout << "finish load" << endl;
 //    img.dump();
-    img.dump_perimeter(Vec2<int>(0,0));
+//    img.dump_perimeter(Vec2<int>(0,0));
     solver<SimpleAdjacencyEvaluator, SimpleSolver>(img, settings);
 }
